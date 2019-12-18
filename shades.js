68f7f0fea5f32bb257f38c277b5f97d42f7a92bd
Art Navsegda <artnavsegda@gmail.com>
2019-12-18 11:26:44 +0300

    Update shades.js

--- shades.js
+++ shades.js
@@ -1,3 +1,12 @@
+var shades = [
+  {name: "bathroom", dev:2 out:2, in: {addr:209, dev:1, up:1, down:2}},
+  {name: "livingroom one", dev:2, out:2, in: {addr:211, dev:1, up:10, down:11}},
+  {name: "livingroom two", dev:2, out:3, in: {addr:209, dev:1, up:3, down:4}},
+  {name: "kitchen", dev:2, out:4, in: {addr:209, dev:1, up:5, down:6}},
+  {name: "cabinet", dev:3, out:1, in: {addr:209, dev:1, up:9, down:10}},
+  {name: "bedroom", dev:3, out:2, in: {addr:209, dev:1, up:11, down:12}}
+];
+
 defineVirtualDevice("shade one", {
     title: "control shade one",
     cells: {
