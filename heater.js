50d088fba07117b2addf2450ac297b68ce0874ba
Art Navsegda <artnavsegda@gmail.com>
2019-12-28 14:57:57 +0300

    okay

--- heater.js
+++ heater.js
@@ -1,174 +1,91 @@
 var heaters = [
-  {name: "balcony", sensor: "28-000008f2ce1a"},
-  {name: "bathroom", sensor: "28-00000a42fdb0"},
-  {name: "livingroom", sensor: "28-00000a41fa1c"},
-  {name: "kitchen", sensor: "28-00000a420768"},
-  {name: "cabinet", sensor: "28-00000a42bbf3"},
-  {name: "bedroom", sensor: "28-00000a4054fa"}
+  {name: "balcony", sensor: "28-000008f2ce1a", out: 1},
+  {name: "bathroom", sensor: "28-00000a42fdb0", out: 2},
+  {name: "livingroom", sensor: "28-00000a41fa1c", out: 4},
+  {name: "kitchen", sensor: "28-00000a420768", out: 5},
+  {name: "cabinet", sensor: "28-00000a42bbf3", out: 6},
+  {name: "bedroom", sensor: "28-00000a4054fa", out: 7}
 ];
 
-
-
-//balcony
-
-function heater_control()
+function heater_control(device, sensor, out)
 {
-  if (dev["heater_control"]["active"] == 1) {
-    if ( dev["wb-w1"]["28-000008f2ce1a"] > dev["heater_control"]["target_temperature"]) {
-      dev["wb-mio-gpio_209:5"]["K2"] = 0;
+  if (dev[device]["active"] == 1) {
+    if ( dev["wb-w1"][sensor] > dev[device]["target_temperature"]) {
+      dev["wb-mio-gpio_209:5"]["K" + out] = 0;
     } else {
-      dev["wb-mio-gpio_209:5"]["K2"] = 1;
+      dev["wb-mio-gpio_209:5"]["K" + out] = 1;
     }
   }
   else {
-    dev["wb-mio-gpio_209:5"]["K2"] = 0;
+    dev["wb-mio-gpio_209:5"]["K" + out] = 0;
   }
 }
 
-defineRule("heater_control_rule", {
-  whenChanged: "wb-w1/28-000008f2ce1a",
-  then: function (newValue, devName, cellName) {
-    heater_control();
-  }
-});
-
-defineRule("heater_control_rule_back", {
-  whenChanged: "heater_control/target_temperature",
-  then: function (newValue, devName, cellName) {
-    heater_control();
-  }
-});
-
-defineRule("heater_control_rule_active", {
-  whenChanged: "heater_control/active",
-  then: function (newValue, devName, cellName) {
-    heater_control();
-  }
-});
-
-defineVirtualDevice("heater_control", {
-    title: "Heater control",
-    cells: {
-      target_temperature: {
-          type: "range",
-          value : 22,
-          max : 100
-      },
-      active: {
+heaters.forEach(function (v) {
+  //log(v);
+  defineVirtualDevice(v.name + "_heater_control", {
+      title: v.name + " heater control",
+      cells: {
+        target_temperature: {
             type: "range",
-            value : 1,
-            max : 1
+            value : 22,
+            max : 100
         },
-      current: {
-          type: "range",
-          value: 1,
-          max: 3,
-          readonly: true
-      },
-      target: {
-          type: "range",
-          value: 0,
-          max: 2
+        active: {
+              type: "range",
+              value : 1,
+              max : 1
+          },
+        current: {
+            type: "range",
+            value: 1,
+            max: 3,
+            readonly: true
+        },
+        target: {
+            type: "range",
+            value: 0,
+            max: 2
+        }
+      }
+  });
+
+  defineRule(v.name + "_heater_control_rule", {
+    whenChanged: "wb-w1/" + v.sensor,
+    then: function (newValue, devName, cellName) {
+      heater_control(v.name + "_heater_control", v.sensor, v.out);
+    }
+  });
+
+  defineRule(v.name + "_heater_control_rule_back", {
+    whenChanged: v.name + "_heater_control/target_temperature",
+    then: function (newValue, devName, cellName) {
+      heater_control(v.name + "_heater_control", v.sensor, v.out);
+    }
+  });
+
+  defineRule(v.name + "_heater_control_rule_active", {
+    whenChanged: v.name + "_heater_control/active",
+    then: function (newValue, devName, cellName) {
+      heater_control();
+    }
+  });
+
+  defineRule(v.name + "_heater_control_target", {
+    whenChanged: v.name + "_heater_control/target",
+    then: function (newValue, devName, cellName)  {
+      switch(newValue)
+      {
+        case 0:
+          dev[v.name + "_heater_control"]["current"] = 1;
+         break;
+        case 1:
+          dev[v.name + "_heater_control"]["current"] = 2;
+         break;
+        case 2:
+          dev[v.name + "_heater_control"]["current"] = 3;
+        break;
       }
     }
-});
-
-defineRule("heater_control_target", {
-  whenChanged: "heater_control/target",
-  then: function (newValue, devName, cellName)  {
-    switch(newValue)
-    {
-      case 0:
-        dev["heater_control"]["current"] = 1;
-       break;
-      case 1:
-        dev["heater_control"]["current"] = 2;
-       break;
-      case 2:
-        dev["heater_control"]["current"] = 3;
-      break;
-    }
-  }
-});
-
-//bath
-
-function heater_control_bath()
-{
-  if (dev["heater_control_bath"]["active"] == 1) {
-    if ( dev["wb-w1"]["28-00000a42fdb0"] > dev["heater_control_bath"]["target_temperature"]) {
-      dev["wb-mio-gpio_209:5"]["K1"] = 0;
-    } else {
-      dev["wb-mio-gpio_209:5"]["K1"] = 1;
-    }
-  }
-  else {
-    dev["wb-mio-gpio_209:5"]["K1"] = 0;
-  }
-}
-
-defineRule("heater_control_bath_rule", {
-  whenChanged: "wb-w1/28-00000a42fdb0",
-  then: function (newValue, devName, cellName) {
-    heater_control_bath();
-  }
-});
-
-defineRule("heater_control_bath_rule_back", {
-  whenChanged: "heater_control_bath/target_temperature",
-  then: function (newValue, devName, cellName) {
-    heater_control_bath();
-  }
-});
-
-defineRule("heater_control_bath_rule_active", {
-  whenChanged: "heater_control_bath/active",
-  then: function (newValue, devName, cellName) {
-    heater_control_bath();
-  }
-});
-
-defineVirtualDevice("heater_control_bath", {
-    title: "Heater control bathroom",
-    cells: {
-      target_temperature: {
-          type: "range",
-          value : 22,
-          max : 100
-      },
-      active: {
-            type: "range",
-            value : 1,
-            max : 1
-        },
-      current: {
-          type: "range",
-          value: 1,
-          max: 3,
-          readonly: true
-      },
-      target: {
-          type: "range",
-          value: 0,
-          max: 2
-      }
-    }
-});
-
-defineRule("heater_control_bath_target", {
-  whenChanged: "heater_control_bath/target",
-  then: function (newValue, devName, cellName)  {
-    switch(newValue)
-    {
-      case 0:
-        dev["heater_control_bath"]["current"] = 1;
-       break;
-      case 1:
-        dev["heater_control_bath"]["current"] = 2;
-       break;
-      case 2:
-        dev["heater_control_bath"]["current"] = 3;
-      break;
-    }
-  }
+  });
 });
--- rules.js
+++ rules.js
@@ -1,178 +1,14 @@
 // place your rules here or add more .js files in this directory
 log("reload");
 
-//balcony
-
-function heater_control()
-{
-  if (dev["heater_control"]["active"] == 1) {
-    if ( dev["wb-w1"]["28-000008f2ce1a"] > dev["heater_control"]["target_temperature"]) {
-      dev["wb-mio-gpio_209:5"]["K2"] = 0;
-    } else {
-      dev["wb-mio-gpio_209:5"]["K2"] = 1;
-    }
-  }
-  else {
-    dev["wb-mio-gpio_209:5"]["K2"] = 0;
-  }
-}
-
-defineRule("heater_control_rule", {
-  whenChanged: "wb-w1/28-000008f2ce1a",
-  then: function (newValue, devName, cellName) {
-    heater_control();
-  }
-});
-
-defineRule("heater_control_rule_back", {
-  whenChanged: "heater_control/target_temperature",
-  then: function (newValue, devName, cellName) {
-    heater_control();
-  }
-});
-
-defineRule("heater_control_rule_active", {
-  whenChanged: "heater_control/active",
-  then: function (newValue, devName, cellName) {
-    heater_control();
-  }
-});
-
-defineVirtualDevice("heater_control", {
-    title: "Heater control",
-    cells: {
-      target_temperature: {
-          type: "range",
-          value : 22,
-          max : 100
-      },
-      active: {
-            type: "range",
-            value : 1,
-            max : 1
-        },
-      current: {
-          type: "range",
-          value: 1,
-          max: 3,
-          readonly: true
-      },
-      target: {
-          type: "range",
-          value: 0,
-          max: 2
-      }
-    }
-});
-
-defineRule("heater_control_target", {
-  whenChanged: "heater_control/target",
-  then: function (newValue, devName, cellName)  {
-    switch(newValue)
-    {
-      case 0:
-        dev["heater_control"]["current"] = 1;
-       break;
-      case 1:
-        dev["heater_control"]["current"] = 2;
-       break;
-      case 2:
-        dev["heater_control"]["current"] = 3;
-      break;
-    }
-  }
-});
-
-//bath
-
-function heater_control_bath()
-{
-  if (dev["heater_control_bath"]["active"] == 1) {
-    if ( dev["wb-w1"]["28-00000a42fdb0"] > dev["heater_control_bath"]["target_temperature"]) {
-      dev["wb-mio-gpio_209:5"]["K1"] = 0;
-    } else {
-      dev["wb-mio-gpio_209:5"]["K1"] = 1;
-    }
-  }
-  else {
-    dev["wb-mio-gpio_209:5"]["K1"] = 0;
-  }
-}
-
-defineRule("heater_control_bath_rule", {
-  whenChanged: "wb-w1/28-00000a42fdb0",
-  then: function (newValue, devName, cellName) {
-    heater_control_bath();
-  }
-});
-
-defineRule("heater_control_bath_rule_back", {
-  whenChanged: "heater_control_bath/target_temperature",
-  then: function (newValue, devName, cellName) {
-    heater_control_bath();
-  }
-});
-
-defineRule("heater_control_bath_rule_active", {
-  whenChanged: "heater_control_bath/active",
-  then: function (newValue, devName, cellName) {
-    heater_control_bath();
-  }
-});
-
-defineVirtualDevice("heater_control_bath", {
-    title: "Heater control bathroom",
-    cells: {
-      target_temperature: {
-          type: "range",
-          value : 22,
-          max : 100
-      },
-      active: {
-            type: "range",
-            value : 1,
-            max : 1
-        },
-      current: {
-          type: "range",
-          value: 1,
-          max: 3,
-          readonly: true
-      },
-      target: {
-          type: "range",
-          value: 0,
-          max: 2
-      }
-    }
-});
-
-defineRule("heater_control_bath_target", {
-  whenChanged: "heater_control_bath/target",
-  then: function (newValue, devName, cellName)  {
-    switch(newValue)
-    {
-      case 0:
-        dev["heater_control_bath"]["current"] = 1;
-       break;
-      case 1:
-        dev["heater_control_bath"]["current"] = 2;
-       break;
-      case 2:
-        dev["heater_control_bath"]["current"] = 3;
-      break;
-    }
-  }
-});
-
 defineVirtualDevice("co2_alarm", {
     title: "CO2 critical",
     cells: {
-	enabled: {
-	    type: "switch",
-	    value: false,
-      	readonly : true
-	},
+    	enabled: {
+    	    type: "switch",
+    	    value: false,
+          	readonly : true
+    	},
     }
 });
 
@@ -190,11 +26,11 @@ defineRule("co2_critical", {
 defineVirtualDevice("motion_detect", {
     title: "Motion detected",
     cells: {
-	enabled: {
-	    type: "switch",
-	    value: false,
-      	readonly : true
-	},
+    	enabled: {
+    	    type: "switch",
+    	    value: false,
+          	readonly : true
+    	},
     }
 });
 
@@ -205,62 +41,6 @@ defineRule("motion_threshold", {
 		dev["motion_detect"]["enabled"] = 1;
     } else {
       	dev["motion_detect"]["enabled"] = 0;
-    }
-  }
-});
-
-defineVirtualDevice("heatercooler", {
-    title: "control heater cooler device",
-    cells: {
-	active: {
-	    type: "range",
-	    value : 1,
-      	max : 1
-	},
-	temperature: {
-	    type: "temperature",
-	    value : 20,
-      	max : 100,
-      	readonly : true
-	},
-    current: {
-      	type: "range",
-      	value: 1,
-      	max: 3,
-      	readonly: true
-    },
-    target: {
-      	type: "range",
-      	value: 0,
-      	max: 2
-    },
-    coolingThreshold: {
-      	type: "range",
-      	value: 20,
-      	max: 35
-    },
-    heatingThreshold: {
-      	type: "range",
-      	value: 20,
-      	max: 25
-    }
-  }
-});
-
-defineRule("heatercooler_control", {
-  whenChanged: "heatercooler/target",
-  then: function (newValue, devName, cellName)  {
-    switch(newValue)
-    {
-      case 0:
-        dev["heatercooler"]["current"] = 1;
-       break;
-      case 1:
-        dev["heatercooler"]["current"] = 2;
-       break;
-      case 2:
-        dev["heatercooler"]["current"] = 3;
-      break;
     }
   }
 });
@@ -329,16 +109,6 @@ defineRule("thermostat_control", {
   }
 });
 
-defineVirtualDevice("stateless_button", {
-  title: "Control stateless button",
-  cells: {
-	   press: {
-       type: "range",
-       value: 0
-     },
-   }
-});
-
 defineRule("stateless_button_control", {
   whenChanged: "wb-mio-gpio_211:1/Counter 3", //Следим за Input 2
   then: function (newValue, devName, cellName)  {
@@ -352,7 +122,7 @@ defineRule("stateless_button_control", {
         dev["wb-mio-gpio_211:4"]["K6"] = 0;
         dev["wb-mio-gpio_211:4"]["K7"] = 0;
         dev["wb-mio-gpio_211:4"]["K8"] = 0;
-      
+
         dev["wb-mio-gpio_211:5"]["K1"] = 0;
         dev["wb-mio-gpio_211:5"]["K2"] = 0;
         dev["wb-mio-gpio_211:5"]["K3"] = 0;
@@ -361,7 +131,7 @@ defineRule("stateless_button_control", {
         dev["wb-mio-gpio_211:5"]["K6"] = 0;
         dev["wb-mio-gpio_211:5"]["K7"] = 0;
         dev["wb-mio-gpio_211:5"]["K8"] = 0;
-      
+
         dev["wb-mio-gpio_211:6"]["K1"] = 0;
         dev["wb-mio-gpio_211:6"]["K2"] = 0;
         dev["wb-mio-gpio_211:6"]["K3"] = 0;
@@ -370,8 +140,6 @@ defineRule("stateless_button_control", {
         dev["wb-mio-gpio_211:6"]["K6"] = 0;
         dev["wb-mio-gpio_211:6"]["K7"] = 0;
         dev["wb-mio-gpio_211:6"]["K8"] = 0;
-      
-    	dev["stateless_button"]["press"] = 1;
     }
   }
 });
